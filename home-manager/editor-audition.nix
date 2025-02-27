e9d349ccd5ddc8364c3938a6e0ac7edfc7a1f979
Alexis Williams <alexis@typedr.at>
2025-02-27 13:41:26 -0800

    Configure Zed

--- home-manager/editor-audition.nix
+++ home-manager/editor-audition.nix
@@ -7,6 +7,46 @@
 
   programs.zed-editor = {
     enable = true;
+
+    extensions = [
+      "codebook"
+      "dockerfile"
+      "nix"
+      "tokyo-night"
+      "toml"
+    ];
+
+    userSettings = {
+      theme = {
+        mode = "system";
+        light = "Tokyo Night Light";
+        dark = "Tokyo Night Storm";
+      };
+
+      languages = {
+        Nix = {
+          language_servers = ["nixd" "!nil"];
+        };
+      };
+
+      lsp = {
+        nixd = {
+          initialization_options = {
+            formatting = {
+              command = ["alejandra" "--quiet" "--"];
+            };
+          };
+        };
+      };
+
+      format_on_save = "on";
+
+      buffer_font_family = "TX-02";
+      buffer_font_size = 14;
+
+      ui_font_family = "SF Pro Display";
+      ui_font_size = 16;
+    };
   };
 
   home.packages = [pkgs.jetbrains.rust-rover];
