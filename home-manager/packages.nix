887e4e8a6ad8fd35821f6efbb17065eb13994592
Alexis Williams <alexis@typedr.at>
2025-02-26 17:08:48 -0800

    Turn on OpenASAR

--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -1,6 +1,9 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    vesktop
+    (discord.override {
+      withOpenASAR = true;
+      withVencord = true;
+    })
 
     # kubernetes stuff
     kubectl
