f791e764464c11fb34d180eb37d8014a33027330
Alexis Williams <alexis@typedr.at>
2025-03-16 14:40:49 -0700

    Fix OpenRGB

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -126,9 +126,11 @@
 
   environment.systemPackages = with pkgs; [
     ntfs3g
-    openrgb-with-all-plugins
   ];
-  services.hardware.openrgb.enable = true;
+  services.hardware.openrgb = {
+    enable = true;
+    package = pkgs.openrgb-with-all-plugins;
+  };
   boot.kernelModules = ["i2c-dev"];
 
   programs.zsh.enable = true;
