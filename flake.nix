b882909a577b9e915534ccdd4de7a1aec41c4865
Alexis Williams <alexis@typedr.at>
2025-04-22 13:40:07 -0700

    Tune up the flake

--- flake.nix
+++ flake.nix
@@ -1,13 +1,9 @@
 {
-  description = "Your new nix config";
+  description = "@typedrat's NixOS configuration.";
 
   inputs = {
-    # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    # You can access packages and modules from different nixpkgs revs
-    # at the same time. Here's an working example:
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
 
     sops-nix = {
       url = "github:Mic92/sops-nix";
@@ -48,6 +44,8 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    apple-emoji.url = "github:samuelngs/apple-emoji-linux";
+
     typedrat-fonts = {
       url = "git+ssh://git@github.com/typedrat/nix-fonts.git";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -65,23 +63,19 @@
     };
 
     hyprland.url = "github:hyprwm/Hyprland";
-
     hyprlock.url = "github:hyprwm/hyprlock/v0.7.0";
-
     hyprland-plugins = {
       url = "github:hyprwm/hyprland-plugins";
       inputs.hyprland.follows = "hyprland";
     };
-
     pyprland.url = "github:hyprland-community/pyprland";
-
-    zen-browser = {
-      url = "github:0xc000022070/zen-browser-flake";
+    wayland-pipewire-idle-inhibit = {
+      url = "github:rafaelrc7/wayland-pipewire-idle-inhibit";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    wayland-pipewire-idle-inhibit = {
-      url = "github:rafaelrc7/wayland-pipewire-idle-inhibit";
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -101,7 +95,7 @@
     ...
   } @ inputs: let
     inherit (self) outputs;
-    # Supported systems for your flake packages, shell, etc.
+
     systems = [
       "aarch64-linux"
       "i686-linux"
@@ -109,33 +103,20 @@
       "aarch64-darwin"
       "x86_64-darwin"
     ];
-    # This is a function that generates an attribute by calling a function you
-    # pass to it, with each system as an argument
+
     forAllSystems = nixpkgs.lib.genAttrs systems;
   in {
-    # Your custom packages
-    # Accessible through 'nix build', 'nix shell', etc
     packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
-    # Formatter for your nix files, available through 'nix fmt'
-    # Other options beside 'alejandra' include 'nixpkgs-fmt'
     formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
 
-    # Your custom packages and modifications, exported as overlays
     overlays = import ./overlays {inherit inputs;};
-    # Reusable nixos modules you might want to export
-    # These are usually stuff you would upstream into nixpkgs
     nixosModules = import ./modules/nixos;
-    # Reusable home-manager modules you might want to export
-    # These are usually stuff you would upstream into home-manager
     homeManagerModules = import ./modules/home-manager;
 
-    # NixOS configuration entrypoint
-    # Available through 'nixos-rebuild --flake .#your-hostname'
     nixosConfigurations = {
       hyperion = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs outputs;};
         modules = [
-          # > Our main nixos configuration file <
           ./nixos/configuration.nix
 
           sops-nix.nixosModules.sops
