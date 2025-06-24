8eeca1c39f1073a244b6fcecb9348302e832dcca
Alexis Williams <typedrat@users.noreply.github.com>
2025-06-23 17:05:53 -0700

    Add Windows public key

--- users/default.nix
+++ users/default.nix
@@ -49,7 +49,8 @@
         shell = pkgs.zsh;
 
         openssh.authorizedKeys.keys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFCm+qnsWUuTDU6IgvxPAkfe6dnwwomGQXlM9c2yUqlJ"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFCm+qnsWUuTDU6IgvxPAkfe6dnwwomGQXlM9c2yUqlJ awilliams@hyperion"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBjz3PWnehAKNKXGpkDu+Huiyizd/24efmLmJCoct+KP awilliams@hyperion-windows"
           "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBLI5a9axsIGCRFLzb9lviLINzebCWV68O94WlXRnMkEKO8uqLAJHGy2aw8i/rB4TcLfqP5lBvOZn0nCNRTvZIRg= awilliams@ipad"
         ];
         hashedPasswordFile = config.sops.secrets."users/awilliams/hashedPassword".path;
