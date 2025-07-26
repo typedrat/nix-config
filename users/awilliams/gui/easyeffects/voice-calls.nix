fb931d3f54901e9917152c0660f79fdf0781849b
Alexis Williams <alexis@typedr.at>
2025-07-25 18:24:14 -0700

    Add autogain and gate to voice call input

--- users/awilliams/gui/easyeffects/voice-calls.nix
+++ users/awilliams/gui/easyeffects/voice-calls.nix
@@ -4,9 +4,21 @@
       blocklist = [];
 
       plugins_order = [
+        "autogain#0"
         "compressor#0"
         "rnnoise#0"
+        "gate#0"
       ];
+
+      "autogain#0" = {
+        bypass = false;
+        input-gain = 0.0;
+        maximum-history = 15;
+        output-gain = 0.0;
+        reference = "Geometric Mean (MSI)";
+        silence-threshold = -70.0;
+        target = -23.0;
+      };
 
       "compressor#0" = {
         attack = 20.0;
@@ -48,6 +60,37 @@
         output-gain = 0.0;
         release = 50.0;
         vad-thres = 50.0;
+        wet = 0.0;
+      };
+
+      "gate#0" = {
+        attack = 100.0;
+        bypass = false;
+        curve-threshold = -24.0;
+        curve-zone = -6.0;
+        dry = -100.0;
+        hpf-frequency = 10.0;
+        hpf-mode = "off";
+        hysteresis = false;
+        hysteresis-threshold = -12.0;
+        hysteresis-zone = -6.0;
+        input-gain = 0.0;
+        lpf-frequency = 20000.0;
+        lpf-mode = "off";
+        makeup = 0.0;
+        output-gain = 0.0;
+        reduction = -24.0;
+        release = 100.0;
+        sidechain = {
+          input = "Internal";
+          lookahead = 0.0;
+          mode = "RMS";
+          preamp = 0.0;
+          reactivity = 10.0;
+          source = "Middle";
+          stereo-split-source = "Left/Right";
+        };
+        stereo-split = false;
         wet = 0.0;
       };
     };
