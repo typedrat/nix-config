14ae3cd7889964b5d517404c3d43004f27a7ddfc
Alexis Williams <alexis@typedr.at>
2025-08-03 16:00:09 -0700

    Mount work Google Drive with `rclone`

--- users/awilliams/rclone.nix
+++ users/awilliams/rclone.nix
@@ -1,12 +1,15 @@
 {
   pkgs,
+  lib,
   config,
   ...
-}: {
+}: let
+  inherit (lib.attrsets) recursiveUpdate;
+in {
   programs.rclone = {
     enable = true;
 
-    remotes = {
+    remotes = rec {
       b2 = {
         config = {
           type = "b2";
@@ -16,31 +19,54 @@
           key = config.sops.secrets."b2/applicationKey".path;
         };
       };
+
+      workdrive = {
+        config = {
+          type = "drive";
+          service_account_file = config.sops.secrets.work-gdrive-sa-key.path;
+          impersonate = config.accounts.email.accounts.Work.address;
+          scope = "drive";
+        };
+      };
+
+      workdrive-shared = recursiveUpdate workdrive {
+        config.team_drive = "0AEjPQYC7XEWcUk9PVA";
+      };
     };
   };
 
   sops.secrets = {
     "b2/keyId" = {};
     "b2/applicationKey" = {};
-  };
-
-  systemd.user.services.rclone-b2-mount = {
-    Unit = {
-      Description = "Service that connects to Backblaze B2";
-      After = ["network-online.target" "sops-nix.service"];
-    };
-    Install.WantedBy = ["default.target"];
-
-    Service = let
-      b2Dir = "${config.home.homeDirectory}/mnt/b2";
-    in {
-      Type = "simple";
-      ExecStartPre = "/run/current-system/sw/bin/mkdir -p ${b2Dir}";
-      ExecStart = "${pkgs.rclone}/bin/rclone mount --vfs-cache-mode full b2: ${b2Dir}";
-      ExecStop = "/run/current-system/sw/bin/fusermount -u ${b2Dir}";
-      Restart = "on-failure";
-      RestartSec = "10s";
-      Environment = ["PATH=/run/wrappers/bin/:$PATH"];
+    work-gdrive-sa-key = {
+      format = "json";
+      sopsFile = ../../secrets/synapdeck-gdrive.json;
+      key = "";
     };
   };
+
+  systemd.user.services =
+    lib.mapAttrs' (name: remote: {
+      name = "rclone-${name}-mount";
+      value = {
+        Unit = {
+          Description = "Service that connects to ${name} remote";
+          After = ["network-online.target" "sops-nix.service"];
+        };
+        Install.WantedBy = ["default.target"];
+
+        Service = let
+          mountDir = "${config.home.homeDirectory}/mnt/${name}";
+        in {
+          Type = "simple";
+          ExecStartPre = "/run/current-system/sw/bin/mkdir -p ${mountDir}";
+          ExecStart = "${pkgs.rclone}/bin/rclone mount --vfs-cache-mode full ${name}: ${mountDir}";
+          ExecStop = "/run/current-system/sw/bin/fusermount -u ${mountDir}";
+          Restart = "on-failure";
+          RestartSec = "10s";
+          Environment = ["PATH=/run/wrappers/bin/:$PATH"];
+        };
+      };
+    })
+    config.programs.rclone.remotes;
 }
