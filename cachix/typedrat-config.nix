5eb1bcf9f15d330ae8cfe754917d1e86ca09a4b6
Alexis Williams <alexis@typedr.at>
2025-08-24 22:47:52 -0700

    Autobuild and automerge configuration

---
+++ cachix/typedrat-config.nix
@@ -0,0 +1,12 @@
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://attic.thisratis.gay/system"
+      ];
+      trusted-public-keys = [
+        "system:M77aaJtD+YL9GLfJ57AXkCzxkqhcXXHQbFXxjZL9QwE="
+      ];
+    };
+  };
+}
