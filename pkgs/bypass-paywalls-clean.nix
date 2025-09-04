58646063dfc2c54c27fa571010c81d86504895de
Alexis Williams <alexis@typedr.at>
2025-09-03 17:09:44 -0700

    Update Bypass Paywalls Clean

--- pkgs/bypass-paywalls-clean.nix
+++ pkgs/bypass-paywalls-clean.nix
@@ -5,10 +5,10 @@
 }:
 inputs.firefox-addons.lib.${stdenv.hostPlatform.system}.buildFirefoxXpiAddon rec {
   pname = "bypass-paywalls-clean";
-  version = "4.1.6.0";
+  version = "4.2.0.0";
   addonId = "magnolia@12.34";
   url = "https://gitflic.ru/project/magnolia1234/bpc_uploads/blob/raw?file=bypass_paywalls_clean-${version}.xpi";
-  sha256 = "sha256-fEZwSRZlagT6F3JHr3rWo7VIMUHxjX8ffrjiBUYgHbM=";
+  sha256 = "sha256-8FFGG3nFWVVZm2VKp9gYw3cc3LkBo5MEJ5XUPkut1KE=";
   meta = with lib; {
     homepage = "https://twitter.com/Magnolia1234B";
     description = "Bypass Paywalls of (custom) news sites";
