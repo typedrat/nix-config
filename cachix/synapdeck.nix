f3de2fd2fc5a378b6c29a09c4c0ac75a40101e71
Alexis Williams <alexis@typedr.at>
2025-10-20 13:27:29 -0700

    add Synapdeck binary cache

---
+++ cachix/synapdeck.nix
@@ -0,0 +1,12 @@
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://attic.thisratis.gay/synapdeck"
+      ];
+      trusted-public-keys = [
+        "synapdeck:ULsOiTa0h/sCQ1/hRNvo0kiW93z0Kvygve1pwofH8Hs="
+      ];
+    };
+  };
+}
