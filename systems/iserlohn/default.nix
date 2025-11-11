a671d44f3abd21903db0c59eecff76e1ac0c97d5
Alexis Williams <alexis@typedr.at>
2025-11-10 17:21:56 -0800

    Move SillyTavern configuration to separate file
    Extract SillyTavern config from systems/iserlohn/default.nix to
    systems/iserlohn/sillytavern.nix for better modularity.

--- systems/iserlohn/default.nix
+++ systems/iserlohn/default.nix
@@ -9,6 +9,7 @@
     ./home-assistant.nix
     ./nvidia.nix
     ./service-user.nix
+    ./sillytavern.nix
     ./sr-iov.nix
     ./zfspv-pool.nix
   ];
@@ -104,85 +105,6 @@
         enable = true;
         owner = "@typedrat:thisratis.gay";
       };
-
-      sillytavern = {
-        enable = true;
-
-        thumbnails = {
-          enabled = true;
-          format = "png";
-          quality = 100;
-          dimensions = {
-            bg = {
-              width = 240;
-              height = 135;
-            };
-            avatar = {
-              width = 864;
-              height = 1280;
-            };
-          };
-        };
-
-        systemExtensions = {
-          "Extension-QuickPersona" = pkgs.fetchFromGitHub {
-            owner = "SillyTavern";
-            repo = "Extension-QuickPersona";
-            rev = "14a56d168954ad4a941eb1116cfaa654c8ca8f47";
-            sha256 = "sha256-AOJ5UIdJn/8SntlrjPkwWsyxa5gW4xjD8/Xswr+Dmn4=";
-          };
-
-          "Extension-TopInfoBar" = pkgs.fetchFromGitHub {
-            owner = "SillyTavern";
-            repo = "Extension-TopInfoBar";
-            rev = "930be54913d90abd90ebd698ddd81affa924e2e1";
-            sha256 = "sha256-WWAijTFpfzR5/z4MlNKIpRmQhlbjhxmTJ3s4dAal7qE=";
-          };
-
-          "SillyTavern-Dialogue-Colorizer-Plus" = pkgs.fetchFromGitHub {
-            owner = "zerofata";
-            repo = "SillyTavern-Dialogue-Colorizer-Plus";
-            rev = "85c81b5d407d0a1595c8a260d700c4811194ebe7";
-            hash = "sha256-PDacujugRYwWiFbul9bnHbe9OAuSMmkj4Z1yqWpys0s=";
-          };
-
-          "SillyTavern-LALib" = pkgs.fetchFromGitHub {
-            owner = "LenAnderson";
-            repo = "SillyTavern-LALib";
-            rev = "6715da13bd27d53bab2eaf5a2eb2509825723718";
-            hash = "sha256-4mGruD1kJa2asKYOG76ZJtAaHJQ1+pjlts/eEtcEsCQ=";
-          };
-
-          "SillyTavern-MessageSummarize" = pkgs.fetchFromGitHub {
-            owner = "qvink";
-            repo = "SillyTavern-MessageSummarize";
-            rev = "b42da1d2e5fd0b4f6a0c6ce941b4f3c5d60ec89b";
-            hash = "sha256-jc6+cm79Fr68Pj5DJSha42Oe228qJm9d18RN1180wHc=";
-          };
-
-          "SillyTavern-MoonlitEchoesTheme" = pkgs.fetchFromGitHub {
-            owner = "RivelleDays";
-            repo = "SillyTavern-MoonlitEchoesTheme";
-            rev = "16ee42f082eb34485f3ed74c44ea4f638b88254f";
-            hash = "sha256-rq0zN6StAN/2JNe9ESOYv6SfiSAygEoj+nl/j+PhnD8=";
-          };
-
-          "SillyTavern-MoreFlexibleContinues" = pkgs.fetchFromGitHub {
-            owner = "LenAnderson";
-            repo = "SillyTavern-MoreFlexibleContinues";
-            rev = "569ec3662470600b76e3c3d6a2e1713fdbf7adbf";
-            hash = "sha256-N8x9OP6zXIJuF5tkd4fN90Qqe9hBKspARg2UTTXJAMw=";
-          };
-
-          "rewrite-extension" = pkgs.fetchFromGitHub {
-            owner = "splitclover";
-            repo = "rewrite-extension";
-            rev = "57fe65ee37a8b76729627de51001ddcb1af22b6d";
-            hash = "sha256-TgkOCEhbQ3czu7S2QHdXcLODzoP8nHfQWs/fSekh6wc=";
-          };
-        };
-      };
-
     };
 
     zfs = {
