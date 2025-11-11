996475c717ba55913aeaadd284fa161462b31baf
Alexis Williams <alexis@typedr.at>
2025-11-10 17:19:30 -0800

    Move Home Assistant configuration to separate file
    Extract Home Assistant config from systems/iserlohn/default.nix to
    systems/iserlohn/home-assistant.nix for better modularity. Also add
    firewall rule for port 3030 used by Elegoo integration.

--- systems/iserlohn/default.nix
+++ systems/iserlohn/default.nix
@@ -6,6 +6,7 @@
   imports = [
     ./disko-config.nix
     ./hardware-configuration.nix
+    ./home-assistant.nix
     ./nvidia.nix
     ./service-user.nix
     ./sr-iov.nix
@@ -182,74 +183,6 @@
         };
       };
 
-      home-assistant = {
-        enable = true;
-        mqtt.enable = true;
-
-        customComponents = with pkgs.home-assistant-custom-components; [
-          elegoo_printer
-          localtuya
-          waste_collection_schedule
-        ];
-
-        extraComponents = [
-          # Components required to complete the onboarding
-          "analytics"
-          "google_translate"
-          "met"
-          "radio_browser"
-          "shopping_list"
-
-          # Recommended for fast zlib compression
-          # https://www.home-assistant.io/integrations/isal
-          "isal"
-
-          # Apple TV
-          "apple_tv"
-
-          # Denon AVR
-          "denonavr"
-
-          # Electricity Maps
-          "co2signal"
-
-          # Enphase
-          "enphase_envoy"
-
-          # ESPHome
-          "esphome"
-
-          # Magic Home
-          "flux_led"
-
-          # Jellyfin
-          "jellyfin"
-
-          # NWS
-          "nws"
-
-          # SMUD
-          "opower"
-
-          # Vizio TV
-          "vizio"
-        ];
-
-        config = {
-          default_config = {};
-
-          waste_collection_schedule = {
-            sources = [
-              {
-                name = "ics";
-                args = {
-                  url = "!secret waste_collection_schedule_url";
-                };
-              }
-            ];
-          };
-        };
-      };
     };
 
     zfs = {
