16e1882a64510356d68c36f4e78ca96e78158395
Alexis Williams <typedrat@users.noreply.github.com>
2025-11-20 22:58:30 -0800

    Update bypass-paywalls-clean to 4.2.5.1 (#55)
    Co-authored-by: typedrat <1057789+typedrat@users.noreply.github.com>

--- pkgs/bypass-paywalls-clean.nix
+++ pkgs/bypass-paywalls-clean.nix
@@ -5,10 +5,10 @@
 }:
 inputs.firefox-addons.lib.${stdenv.hostPlatform.system}.buildFirefoxXpiAddon rec {
   pname = "bypass-paywalls-clean";
-  version = "4.2.3.0";
+  version = "4.2.5.1";
   addonId = "magnolia@12.34";
   url = "https://gitflic.ru/project/magnolia1234/bpc_uploads/blob/raw?file=bypass_paywalls_clean-${version}.xpi";
-  sha256 = "sha256-5IFIZQQKL8RQuj8C9OSKqyL5wCYcmOu3hEiTV26j1Dk=";
+  sha256 = "sha256-gOodsreoXZCpOsqFnAFzni8E3zwUiosAMeiOCftriW0=";
   meta = with lib; {
     homepage = "https://twitter.com/Magnolia1234B";
     description = "Bypass Paywalls of (custom) news sites";
