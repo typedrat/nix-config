258b4fc7d8596756eb8ee09d7ec02301580de6a0
Alexis Williams <alexis@typedr.at>
2026-01-17 14:48:16 -0800

    Fix accidentally removed program

--- systems/ulysses/default.nix
+++ systems/ulysses/default.nix
@@ -126,6 +126,10 @@
     };
   };
 
+  environment.systemPackages = with pkgs; [
+    llama-cpp
+  ];
+
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "25.05";
 }
