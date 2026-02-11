c5090b1970f34a796b8381bc71ad78af1a0bab5a
Alexis Williams <alexis@typedr.at>
2026-02-11 14:38:42 -0800

    Reinforce the Orca Slicer fixes

--- modules/home-manager/gui/productivity/3d-printing.nix
+++ modules/home-manager/gui/productivity/3d-printing.nix
@@ -6,15 +6,18 @@
 }: let
   inherit (lib.modules) mkIf;
 
-  # https://github.com/NixOS/nixpkgs/issues/468456
   orca-slicer-wrapped = pkgs.symlinkJoin {
     name = "orca-slicer-wrapped";
     paths = [pkgs.orca-slicer];
     buildInputs = [pkgs.makeWrapper];
     postBuild = ''
+      # workaround from NixOS/nixpkgs#468456
       wrapProgram $out/bin/orca-slicer \
-        --prefix MESA_LOADER_DRIVER_OVERRIDE : zink \
-        --prefix __EGL_VENDOR_LIBRARY_FILENAMES : ${pkgs.mesa}/share/glvnd/egl_vendor.d/50_mesa.json
+      --prefix __GLX_VENDOR_LIBRARY_NAME : mesa \
+      --prefix __EGL_VENDOR_LIBRARY_FILENAMES : ${pkgs.mesa}/share/glvnd/egl_vendor.d/50_mesa.json \
+      --prefix MESA_LOADER_DRIVER_OVERRIDE : zink \
+      --prefix GALLIUM_DRIVER : zink \
+      --prefix LIBGL_KOPPER_DRI2 : 1 \
     '';
   };
 in {
