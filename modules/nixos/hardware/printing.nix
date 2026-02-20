dc96e5291a2bc84d8bc6ca1a6066bd1f57e1da45
Alexis Williams <alexis@typedr.at>
2026-02-19 22:09:21 -0800

    Configure my printer

--- modules/nixos/hardware/printing.nix
+++ modules/nixos/hardware/printing.nix
@@ -23,5 +23,17 @@ in {
         pkgs.cups-brother-dcpl2550dw
       ];
     };
+
+    hardware.printers = {
+      ensureDefaultPrinter = "Brother_DCP-L2550DW";
+      ensurePrinters = [
+        {
+          name = "Brother_DCP-L2550DW";
+          description = "Brother DCP-L2550DW";
+          deviceUri = "usb://Brother/DCP-L2550DW%20series?serial=U64966C0N935597";
+          model = "brother-DCPL2550DW-cups-en.ppd";
+        }
+      ];
+    };
   };
 }
