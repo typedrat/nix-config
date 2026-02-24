b80afd16be428f9049503d49dd0fa860519c955c
Alexis Williams <alexis@typedr.at>
2026-02-24 14:36:49 -0800

    Redirect NVIDIA shader cache to XDG cache directory

--- modules/home-manager/cli/xdg-compliance.nix
+++ modules/home-manager/cli/xdg-compliance.nix
@@ -9,42 +9,48 @@
   userCfg = osConfig.rat.users.${username} or {};
   cliCfg = userCfg.cli or {};
 in {
-  config = modules.mkIf (cliCfg.enable or false) {
-    home.sessionVariables = {
-      # Development tools
-      CARGO_HOME = "${config.xdg.dataHome}/cargo";
-      RUSTUP_HOME = "${config.xdg.dataHome}/rustup";
-      BUN_INSTALL = "${config.xdg.dataHome}/bun";
-      NPM_CONFIG_USERCONFIG = "${config.xdg.configHome}/npm/npmrc";
-      npm_config_cache = "${config.xdg.cacheHome}/npm";
-      DOCKER_CONFIG = "${config.xdg.configHome}/docker";
+  config = modules.mkIf (cliCfg.enable or false) (modules.mkMerge [
+    {
+      home.sessionVariables = {
+        # Development tools
+        CARGO_HOME = "${config.xdg.dataHome}/cargo";
+        RUSTUP_HOME = "${config.xdg.dataHome}/rustup";
+        BUN_INSTALL = "${config.xdg.dataHome}/bun";
+        NPM_CONFIG_USERCONFIG = "${config.xdg.configHome}/npm/npmrc";
+        npm_config_cache = "${config.xdg.cacheHome}/npm";
+        DOCKER_CONFIG = "${config.xdg.configHome}/docker";
 
-      # Cloud/Infra
-      TF_CLI_CONFIG_FILE = "${config.xdg.configHome}/terraform/terraformrc";
-      TF_PLUGIN_CACHE_DIR = "${config.xdg.cacheHome}/terraform/plugins";
-      CHECKPOINT_DISABLE = "1";
+        # Cloud/Infra
+        TF_CLI_CONFIG_FILE = "${config.xdg.configHome}/terraform/terraformrc";
+        TF_PLUGIN_CACHE_DIR = "${config.xdg.cacheHome}/terraform/plugins";
+        CHECKPOINT_DISABLE = "1";
 
-      # Shell/REPL history
-      PYTHON_HISTORY = "${config.xdg.stateHome}/python/history";
-      LESSHISTFILE = "${config.xdg.stateHome}/less/history";
-      IPYTHONDIR = "${config.xdg.configHome}/ipython";
+        # Shell/REPL history
+        PYTHON_HISTORY = "${config.xdg.stateHome}/python/history";
+        LESSHISTFILE = "${config.xdg.stateHome}/less/history";
+        IPYTHONDIR = "${config.xdg.configHome}/ipython";
 
-      # Other
-      WINEPREFIX = "${config.xdg.dataHome}/wine";
-    };
+        # Other
+        WINEPREFIX = "${config.xdg.dataHome}/wine";
+      };
 
-    home.sessionPath = [
-      "${config.home.sessionVariables.CARGO_HOME}/bin"
-      "${config.home.sessionVariables.BUN_INSTALL}/bin"
-    ];
+      home.sessionPath = [
+        "${config.home.sessionVariables.CARGO_HOME}/bin"
+        "${config.home.sessionVariables.BUN_INSTALL}/bin"
+      ];
 
-    programs.zsh = {
-      completionInit = ''
-        autoload -U compinit && compinit -d "${config.xdg.cacheHome}/zsh/zcompdump-$ZSH_VERSION"
-      '';
-      history.path = "${config.xdg.stateHome}/zsh/history";
-    };
+      programs.zsh = {
+        completionInit = ''
+          autoload -U compinit && compinit -d "${config.xdg.cacheHome}/zsh/zcompdump-$ZSH_VERSION"
+        '';
+        history.path = "${config.xdg.stateHome}/zsh/history";
+      };
 
-    programs.bash.historyFile = "/dev/null";
-  };
+      programs.bash.historyFile = "/dev/null";
+    }
+
+    (modules.mkIf (osConfig.rat.hardware.nvidia.enable or false) {
+      home.sessionVariables.__GL_SHADER_DISK_CACHE_PATH = "${config.xdg.cacheHome}/nvidia";
+    })
+  ]);
 }
