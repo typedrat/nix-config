a514b85b610c4613503f2b71506ed0e1f1330be5
Alexis Williams <alexis@typedr.at>
2026-04-10 16:37:56 -0700

    authentik: move Chaptarr to Media group

--- terraform/authentik/applications/readarr.nix
+++ terraform/authentik/applications/readarr.nix
@@ -1,10 +1,10 @@
 {
   authentik.applications.chaptarr = {
     name = "Chaptarr";
-    group = "Torrents";
+    group = "Media";
     icon = "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/svg/chaptarr.svg";
     description = "Audiobook and e-book organizer/manager for usenet and torrent users.";
-    accessGroups = ["discord-sysop"];
+    accessGroups = [ "discord-sysop" ];
 
     proxy = {
       externalHost = "https://chaptarr.thisratis.gay";
