574bb85918e3412171054fb2d1f03862251361cb
Alexis Williams <alexis@typedr.at>
2026-04-10 17:31:33 -0700

    zipline: declare OAUTH_BYPASS_LOCAL_LOGIN and OAUTH_LOGIN_ONLY settings

--- modules/nixos/services/media/zipline.nix
+++ modules/nixos/services/media/zipline.nix
@@ -2,7 +2,8 @@
   config,
   lib,
   ...
-}: let
+}:
+let
   inherit (lib) modules options types;
   cfg = config.rat.services.zipline;
   impermanenceCfg = config.rat.impermanence;
@@ -11,7 +12,8 @@
   inherit (config.rat.services) domainName;
 
   domain = "${cfg.subdomain}.${domainName}";
-in {
+in
+{
   options.rat.services.zipline = {
     enable = options.mkEnableOption "Zipline";
     subdomain = options.mkOption {
@@ -47,6 +49,8 @@ in {
           DATASOURCE_LOCAL_DIRECTORY = cfg.uploadsDir;
 
           FEATURES_OAUTH_REGISTRATION = "true";
+          OAUTH_BYPASS_LOCAL_LOGIN = "true";
+          OAUTH_LOGIN_ONLY = "true";
           OAUTH_OIDC_AUTHORIZE_URL = "https://${authentikCfg.subdomain}.${domainName}/application/o/authorize/";
           OAUTH_OIDC_USERINFO_URL = "https://${authentikCfg.subdomain}.${domainName}/application/o/userinfo/";
           OAUTH_OIDC_TOKEN_URL = "https://${authentikCfg.subdomain}.${domainName}/application/o/token/";
@@ -61,19 +65,19 @@ in {
       sops.secrets."zipline/coreSecret" = {
         sopsFile = ../../../../secrets/zipline.yaml;
         key = "coreSecret";
-        restartUnits = ["zipline.service"];
+        restartUnits = [ "zipline.service" ];
       };
 
       sops.secrets."zipline/clientId" = {
         sopsFile = ../../../../secrets/zipline.yaml;
         key = "clientId";
-        restartUnits = ["zipline.service"];
+        restartUnits = [ "zipline.service" ];
       };
 
       sops.secrets."zipline/clientSecret" = {
         sopsFile = ../../../../secrets/zipline.yaml;
         key = "clientSecret";
-        restartUnits = ["zipline.service"];
+        restartUnits = [ "zipline.service" ];
       };
 
       sops.secrets."zipline/db/password" = {
@@ -81,30 +85,32 @@ in {
         key = "db/password";
         owner = "postgres";
         group = "postgres";
-        restartUnits = ["zipline.service"];
+        restartUnits = [ "zipline.service" ];
       };
 
       sops.templates."zipline.env" = {
         content = ''
           CORE_SECRET=${config.sops.placeholder."zipline/coreSecret"}
-          DATABASE_URL=postgresql://zipline:${config.sops.placeholder."zipline/db/password"}@localhost:${toString config.links.postgres.port}/zipline
+          DATABASE_URL=postgresql://zipline:${
+            config.sops.placeholder."zipline/db/password"
+          }@localhost:${toString config.links.postgres.port}/zipline
           OAUTH_OIDC_CLIENT_ID=${config.sops.placeholder."zipline/clientId"}
           OAUTH_OIDC_CLIENT_SECRET=${config.sops.placeholder."zipline/clientSecret"}
         '';
-        restartUnits = ["zipline.service"];
+        restartUnits = [ "zipline.service" ];
       };
 
       rat.services.postgres = {
         enable = true;
         users.zipline = {
           passwordFile = config.sops.secrets."zipline/db/password".path;
-          ownedDatabases = ["zipline"];
+          ownedDatabases = [ "zipline" ];
         };
       };
 
       systemd.services.zipline = {
-        after = ["postgresql.service"];
-        requires = ["postgresql.service"];
+        after = [ "postgresql.service" ];
+        requires = [ "postgresql.service" ];
         serviceConfig.DynamicUser = lib.mkForce false;
       };
 
@@ -113,7 +119,7 @@ in {
         group = "zipline";
         home = "/var/lib/zipline";
       };
-      users.groups.zipline = {};
+      users.groups.zipline = { };
 
       rat.services.traefik.routes.zipline = {
         enable = true;
