b30df86db74247a6b39b35d0aefa025b70784edd
Alexis Williams <alexis@typedr.at>
2026-08-04 11:21:23 -0700

    home-manager: add rat.userSecrets for per-user sops secrets

--- modules/home-manager/core/sops.nix
+++ modules/home-manager/core/sops.nix
@@ -5,20 +5,71 @@
   lib,
   ...
 }: let
-  inherit (lib.modules) mkIf;
+  inherit (lib) attrsets modules options types;
+  inherit (config.home) username;
   impermanenceCfg = osConfig.rat.impermanence;
   inherit (impermanenceCfg) persistDir;
+
+  userSecretsDir = ../../../secrets + "/${username}";
+  cfg = config.rat.userSecrets;
+
+  secretFile = file: userSecretsDir + "/${file}.yaml";
+
+  # Secrets are named "<file>/<key>" so that two files can declare the same key
+  # without colliding in the flat sops.secrets namespace.
+  fileSecrets = file:
+    attrsets.mapAttrs' (
+      key: secretCfg:
+        attrsets.nameValuePair "${file}/${key}" ({
+            sopsFile = secretFile file;
+            inherit key;
+          }
+          // secretCfg)
+    );
 in {
   imports = [
     inputs.sops-nix.homeManagerModules.sops
   ];
 
-  sops = {
-    defaultSopsFile = ../../../secrets/default.yaml;
-    age.sshKeyPaths = ["${config.home.homeDirectory}/.ssh/id_ed25519"];
+  options.rat.userSecrets = options.mkOption {
+    type = types.attrsOf (types.attrsOf (types.submodule {
+      freeformType = types.attrsOf types.anything;
+    }));
+    default = {};
+    example = options.literalExpression ''
+      {
+        anki = {
+          username = {};
+          syncKey = {};
+        };
+      }
+    '';
+    description = ''
+      Secrets belonging to this user alone, declared as
+      `rat.userSecrets.<file>.<key>`. Each entry decrypts `<key>` out of
+      `secrets/<username>/<file>.yaml` and exposes it as
+      `config.sops.secrets."<file>/<key>"`. Attributes set on an entry are
+      passed through to the underlying sops secret.
+    '';
   };
 
-  home.persistence.${persistDir} = mkIf impermanenceCfg.home.enable {
-    directories = [".config/sops" ".config/sops-nix"];
+  config = {
+    sops = {
+      defaultSopsFile = ../../../secrets/default.yaml;
+      age.sshKeyPaths = ["${config.home.homeDirectory}/.ssh/id_ed25519"];
+
+      secrets = attrsets.concatMapAttrs fileSecrets cfg;
+    };
+
+    assertions =
+      attrsets.mapAttrsToList (file: _: {
+        assertion = builtins.pathExists (secretFile file);
+        message = "rat.userSecrets.${file} needs secrets/${username}/${file}.yaml, which does not exist.";
+      })
+      cfg;
+
+    home.persistence.${persistDir} = modules.mkIf impermanenceCfg.home.enable {
+      directories = [".config/sops" ".config/sops-nix"];
+    };
   };
 }
